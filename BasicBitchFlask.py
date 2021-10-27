1a895344c587ced7aa624bfc5c6112260a1f503b
Ubuntu <ubuntu@ip-172-31-19-87.us-west-1.compute.internal>
2021-10-27 01:57:03 +0000

    wtf

--- BasicBitchFlask.py
+++ BasicBitchFlask.py
@@ -1,35 +1,43 @@
-from flask import Flask, send_from_directory, request, send_file, jsonify, make_response
+from flask import Flask, send_from_directory, request, send_file, jsonify, make_response, redirect
 from markupsafe import escape
 import sqlite3
 
-connection = sqlite3.connect(":memory:")
+#connection = sqlite3.connect(":memory:")
 app = Flask(__name__)
 connection = sqlite3.connect("CookieCount.db")
 cursor=connection.cursor()
 cursor.execute("CREATE TABLE IF NOT EXISTS CookieSheet (species TEXT NOT NULL PRIMARY KEY, current_votes INTEGER, UNIQUE(species, current_votes))")
 
+#@app.route("/")
+#def hello():
+#	return redirect("https://robotsbyryan.com/Cookies", code=302)
+
 @app.route("/")
 def hello_world():
-	return "<p>Hello, World!</p>"
+	return send_from_directory('static/Cookies', 'index.html')
 
 @app.route("/cookies")
 def display_cookies():
 	return "Delicious cookies here soon!"
 
-@app.route("/path/<path:subpath>")
-def return_image(subpath):
+@app.route("/Cookies/<path:subpath>")
+def return_file(subpath):
 	#return "COOKIE PLZ"
 	#return send_file('static/Cookies/cookie300.png')
-	return send_from_directory('static/Cookies', f'{escape(subpath)}')
+	print(escape(subpath))
+	return send_from_directory('static/Cookies', escape(subpath))
 
-@app.route("/cookiechoice/<string:cookiechoice>")
+@app.route('/Cookies/cookiechoice/<string:cookiechoice>', methods=['POST'])
 def update_baking_sheet(cookiechoice):
-	cursor.execute("INSERT OR IGNORE INTO CookieSheet VALUES (?, '1')", [cookiechoice])
-	cursor.execute("UPDATE CookieSheet SET current_votes = current_votes + 1 WHERE species = ?", [cookiechoice])
-	connection.commit()
-	return make_response('nice', 200) #https://stackoverflow.com/questions/26079754/flask-how-to-return-a-success-status-code-for-ajax-call
+	if cookiechoice in ["Chocolate_Chip", "Candy", "Oatmeal_Raisin"]:
+		cursor.execute("INSERT OR IGNORE INTO CookieSheet VALUES (?, '1')", [cookiechoice])
+		cursor.execute("UPDATE CookieSheet SET current_votes = current_votes + 1 WHERE species = ?", [cookiechoice])
+		connection.commit()
+		response = make_response()
+		response.status_code = 200
+		return response #https://stackoverflow.com/questions/26079754/flask-how-to-return-a-success-status-code-for-ajax-call
 
-@app.route("/topcookies")
+@app.route("/Cookies/topcookies")
 def current_favorites():
 	c = cursor.execute("Select species, current_votes FROM CookieSheet ORDER BY current_votes DESC").fetchall()
 	_cookie1 = c[0][0]
--- Sqlite3Playground.py
+++ Sqlite3Playground.py
@@ -3,13 +3,14 @@ import requests
 import time
 
 begin = time.perf_counter()
-for i in range (500):
-    requests.get('http://54.176.96.251/cookiechoice/Oreo')
+requests.post('http://54.176.96.251/Cookies/cookiechoice/Oatmeal_Raisin')
+requests.post('http://54.176.96.251/Cookies/cookiechoice/Candy')
+requests.post('http://54.176.96.251/Cookies/cookiechoice/Chocolate_Chip')
 end = time.perf_counter()
 print(f"Done! Time taken: {end-begin:.4f} seconds") #https://realpython.com/python-timer/
 connection = sqlite3.connect("CookieCount.db")
 cursor=connection.cursor()
-cursor.execute("UPDATE CookieSheet SET current_votes = 0 WHERE species = 'MandM'") 
+#cursor.execute("UPDATE CookieSheet SET current_votes = 0 WHERE species = 'MandM'") 
 shouldbe50 = cursor.execute("SELECT * FROM CookieSheet").fetchall()
 connection.commit()
 connection.close()
