1cd1d8c8dbf2e617bba793d726faf56f5166e3c6
Ubuntu <ubuntu@ip-172-31-19-87.us-west-1.compute.internal>
2021-10-29 00:08:34 +0000

    more huge security errors

--- Sqlite3Playground.py
+++ Sqlite3Playground.py
@@ -2,15 +2,19 @@ import sqlite3
 import requests
 import time
 
-begin = time.perf_counter()
-requests.post('http://54.176.96.251/Cookies/cookiechoice/Oatmeal_Raisin')
-requests.post('http://54.176.96.251/Cookies/cookiechoice/Candy')
-requests.post('http://54.176.96.251/Cookies/cookiechoice/Chocolate_Chip')
+'''begin = time.perf_counter()
+requests.post('https://robotsbyryan.com/Cookies/cookiechoice/Oatmeal_Raisin')
+requests.post('https://robotsbyryan.com/Cookies/cookiechoice/Candy')
+requests.post('https://robotsbyryan.com/Cookies/cookiechoice/Chocolate_Chip')
 end = time.perf_counter()
 print(f"Done! Time taken: {end-begin:.4f} seconds") #https://realpython.com/python-timer/
+'''
 connection = sqlite3.connect("CookieCount.db")
 cursor=connection.cursor()
-#cursor.execute("UPDATE CookieSheet SET current_votes = 0 WHERE species = 'MandM'") 
+'''list = ['Oreo']#["Strawberry", "Falafel", "MandM", "Reeses", "Chocolate Chip"]
+for cookie in list:
+	cursor.execute("DELETE FROM CookieSheet WHERE species = ?", [cookie]) 
+'''
 shouldbe50 = cursor.execute("SELECT * FROM CookieSheet").fetchall()
 connection.commit()
 connection.close()
