06c765356c22ca5496b8c5686fe456e2b63dec23
Ryan Gauthier <ryangauthierformal@gmail.com>
2026-04-17 00:56:24 -0700

    whoops

--- functions/counter.js
+++ functions/counter.js
@@ -1,7 +1,7 @@
 export async function onRequest(context) {
     try {
         const ps = context.env.COUNTERTABLE.prepare("Select count FROM counter WHERE counterID = 0");
-        const returnValue = await stmt.run();
+        const returnValue = await ps.run();
         return Response.json(returnValue);
     } catch (e) {
         console.error({message: e.message});
