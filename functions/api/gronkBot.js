77d559a497787555b67832b1cbfe4a2ee7f9931e
Ryan Gauthier <ryangauthierformal@gmail.com>
2026-05-09 00:46:42 -0700

    Update gronkBot.js
    small fixes...

--- functions/api/gronkBot.js
+++ functions/api/gronkBot.js
@@ -1,6 +1,7 @@
 export async function onRequestPost(context) {
 //Step 0: Check if request has required authentication
 if (context.request.headers.has("X-Signature-Ed25519") && context.request.headers.has("X-Signature-Timestamp")){
+    console.log("Had headers!");
     const temp_PUBLIC_KEY = context.env.D_PUBKEY;
     //Step 1: Convert the raw public key to a CryptoKey, https://developer.mozilla.org/en-US/docs/Web/API/CryptoKey
     const PUBLIC_KEY = await crypto.subtle.importKey('raw', Uint8Array.fromHex(temp_PUBLIC_KEY),
@@ -21,11 +22,12 @@ if (context.request.headers.has("X-Signature-Ed25519") && context.request.header
     //Step 3: Call crypto.subtle.verify
     let isVerified = await crypto.subtle.verify({ "name": "Ed25519" },PUBLIC_KEY,signature,data);
     if (!isVerified) {
+        console.log("Invalid request signature or crypto failed...");
         return new Response("Invalid request signature!",{status: 401});
     }else{
         //Step 4: Actually handle a given request
         //Check type
-        const checkType = await context.request.json()
+        const checkType = await context.request.json();
         if ("type" in checkType){
             if(checkType["type"] == 1){
                 //It's just a ping, return pong
@@ -33,7 +35,7 @@ if (context.request.headers.has("X-Signature-Ed25519") && context.request.header
             }
             else if(checkType["type"] == 2){
                 //It's a valid interaction, do something
-                return Response.json({type: 4, data: {content: "Hello, I'm Gronk. I'm a work in progress!"}})
+                return Response.json({type: 4, data: {content: "Hello, I'm Gronk. I'm a work in progress!"}});
             }
             else{
                 console.log("Type present but supported, weird...");
@@ -45,7 +47,7 @@ if (context.request.headers.has("X-Signature-Ed25519") && context.request.header
         }
     }         
 }else{
-    return Response("Missing headers...",{status: 401})
+    return new Response("Missing headers...",{status: 401})
 }
     //https://docs.discord.com/developers/interactions/overview#validating-security-headers
     //https://github.com/discord/discord-interactions-js/blob/main/src/index.ts
